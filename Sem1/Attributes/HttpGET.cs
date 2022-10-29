134635da1220a840129dd4e3285e6600abfdaaf4
h1roman <hiromanosu@gmail.com>
2022-10-29 12:15:25 +0300

    sem1 week1

---
+++ Sem1/Attributes/HttpGET.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HttpServer.Attributes
+{
+    public class HttpGET : Attribute
+    {
+        public string MethodURI;
+
+        public HttpGET(string methodUri)
+        {
+            MethodURI = methodUri;
+        }
+    }
+}
