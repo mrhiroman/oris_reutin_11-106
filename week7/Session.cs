ee14b81bca3008575bfe297e20ab46f1fb83bbc8
h1roman <hiromanosu@gmail.com>
2022-11-14 14:07:35 +0300

    commit 5 add SessionManager

---
+++ week7/Session.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HttpServer
+{
+    public class Session
+    {
+        public Guid Id { get; private set; }
+        public int AccountId { get; private set; }
+        public string Login { get; private set; }
+        public DateTime CreateDateTime { get; private set; }
+
+        public Session(Guid id, int accountId, string login)
+        {
+            Id = id;
+            AccountId = accountId;
+            Login = login;
+            CreateDateTime = DateTime.Now;
+        }
+    }
+}
---
+++ week7/SessionManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.Caching;
+
+namespace HttpServer
+{
+    public class SessionManager
+    {
+        ObjectCache _cache = MemoryCache.Default;
+        
+        public void CreateSession(int accountId, string login)
+        {
+            var session = new Session(Guid.NewGuid(), accountId, login);
+            _cache.Set(session.Id.ToString(), session, DateTimeOffset.Now.AddMinutes(2));
+        }
+
+        public bool ValidateSession(int sessionId)
+        {
+            var session = GetInformation(sessionId);
+            if (DateTime.Now - TimeSpan.FromMinutes(15) >= session.CreateDateTime) return true;
+            return false;
+        }
+
+        public Session GetInformation(int sessionId)
+        {
+            var session = _cache.Get(sessionId.ToString());
+            return (Session)session;
+        }
+
+        public void UpdateSession(int sessionId)
+        {
+            var session = GetInformation(sessionId);
+            if(ValidateSession(sessionId)) _cache.Set(session.Id.ToString(), session, DateTimeOffset.Now.AddMinutes(2));
+        }
+    }
+}
